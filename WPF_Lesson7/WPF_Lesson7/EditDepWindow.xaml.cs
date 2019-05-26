63e9232ca4755a760b0e1068b8ca0990581aaa09
Dant777 <kolbt87@gmail.com>
2019-05-27 01:57:33 +0300

    Готовые 2 стака

---
+++ WPF_Lesson7/WPF_Lesson7/EditDepWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Lesson7
+{
+    /// <summary>
+    /// Логика взаимодействия для EditDepWindow.xaml
+    /// </summary>
+    public partial class EditDepWindow : Window
+    {
+        public DataRow resultRow { get; set; }
+
+        public EditDepWindow(DataRow dataRow)
+        {
+            InitializeComponent();
+            resultRow = dataRow;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtAddNameDep.Text = resultRow["Name"].ToString();
+            
+        }
+
+        private void BtnAddOK_Click(object sender, RoutedEventArgs e)
+        {
+            resultRow["Name"] = txtAddNameDep.Text;
+            this.DialogResult = true;
+        }
+
+        private void BtnAddCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
--- WPF_Lesson7/WPF_Lesson7/MainWindow.xaml.cs
+++ WPF_Lesson7/WPF_Lesson7/MainWindow.xaml.cs
@@ -25,8 +25,12 @@ namespace WPF_Lesson7
     {
 
         SqlConnection connection;
-        SqlDataAdapter adapter;
-        DataTable dt;
+        //Employee
+        SqlDataAdapter adapterEmp;
+        DataTable dtEmp;
+        //Department
+        SqlDataAdapter adapterDep;
+        DataTable dtDep;
 
         public MainWindow()
         {
@@ -37,72 +41,109 @@ namespace WPF_Lesson7
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //Employee
             connection = new SqlConnection(connectionString);
-            adapter = new SqlDataAdapter(); //адаптер    
-            
+            adapterEmp = new SqlDataAdapter(); //адаптер    
+            //Department
+            adapterDep = new SqlDataAdapter();
+
             #region Select
-
+            //Employee
             var sql = "SELECT Id, FIO, Age, Salary FROM Employee";//sql запрос
-            SqlCommand command = new SqlCommand(sql, connection);
-            adapter.SelectCommand = command;
+            SqlCommand commandEmp = new SqlCommand(sql, connection);
+            adapterEmp.SelectCommand = commandEmp;
 
+            //Department
+            SqlCommand commandDep = new SqlCommand("SELECT Id, Name FROM Department", connection);
+            adapterDep.SelectCommand = commandDep;
             #endregion
 
             #region Insert
-            command = new SqlCommand(@"INSERT INTO Employee (FIO, Age, Salary) 
+            //Employee
+            commandEmp = new SqlCommand(@"INSERT INTO Employee (FIO, Age, Salary) 
                           VALUES (@FIO, @Age, @Salary); SET @ID = @@IDENTITY;",
               connection);
 
-            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
-            command.Parameters.Add("@Age", SqlDbType.NVarChar, -1, "Age");
-            command.Parameters.Add("@Salary", SqlDbType.NVarChar, 58, "Salary");
+            commandEmp.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            commandEmp.Parameters.Add("@Age", SqlDbType.NVarChar, -1, "Age");
+            commandEmp.Parameters.Add("@Salary", SqlDbType.NVarChar, 58, "Salary");
 
-            SqlParameter param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
+            SqlParameter param = commandEmp.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
 
             param.Direction = ParameterDirection.Output;
 
-            adapter.InsertCommand = command;
+            adapterEmp.InsertCommand = commandEmp;
+
+            //Department
+            commandDep = new SqlCommand(@"INSERT INTO Department (Name) 
+                          VALUES (@Name); SET @ID = @@IDENTITY;",
+              connection);
+            commandDep.Parameters.Add("@Name", SqlDbType.NVarChar, -1, "Name");
+
+            SqlParameter paramDep = commandDep.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
+            paramDep.Direction = ParameterDirection.Output;
+            adapterDep.InsertCommand = commandDep;
             #endregion
 
             #region Update
-            command = new SqlCommand(@"UPDATE Employee SET FIO = @FIO,
+            //Employee
+            commandEmp = new SqlCommand(@"UPDATE Employee SET FIO = @FIO,
                 Age = @Age, Salary = @Salary WHERE ID = @ID", connection);
 
-            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
-            command.Parameters.Add("@Age", SqlDbType.NVarChar, -1, "Age");
-            command.Parameters.Add("@Salary", SqlDbType.NVarChar, -1, "Salary");
+            commandEmp.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            commandEmp.Parameters.Add("@Age", SqlDbType.NVarChar, -1, "Age");
+            commandEmp.Parameters.Add("@Salary", SqlDbType.NVarChar, -1, "Salary");
             
-            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param = commandEmp.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
 
             param.SourceVersion = DataRowVersion.Original;
 
-            adapter.UpdateCommand = command;
+            adapterEmp.UpdateCommand = commandEmp;
+
+            //Department
+            commandDep = new SqlCommand(@"UPDATE Department SET Name = @Name WHERE ID = @ID", 
+              connection);
+            commandDep.Parameters.Add("@Name", SqlDbType.NVarChar, -1, "Name");
+            paramDep = commandDep.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
+            paramDep.SourceVersion = DataRowVersion.Original;
+            adapterDep.UpdateCommand = commandDep;
             #endregion
 
             #region delete
-
-            command = new SqlCommand("DELETE FROM Employee WHERE Id = @ID", connection);
-            SqlParameter parameter = command.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
-            adapter.DeleteCommand = command;
-
+            //Employee
+            commandEmp = new SqlCommand("DELETE FROM Employee WHERE Id = @ID", connection);
+            SqlParameter parameter = commandEmp.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
+            adapterEmp.DeleteCommand = commandEmp;
+            // Department
+            commandDep = new SqlCommand("DELETE FROM Department WHERE Id = @ID", connection);
+            SqlParameter parameterDep = commandDep.Parameters.Add("@ID", SqlDbType.Int, 0, "Id");
+            adapterDep.DeleteCommand = commandDep;
             #endregion
+            //Employee
+            dtEmp = new DataTable(); // прослойка которая сцепляет видимую таблицу(View) и адаптер
+            adapterEmp.Fill(dtEmp);
+            
+            EmployeeDataGrid.DataContext = dtEmp.DefaultView;
 
-            dt = new DataTable(); // прослойка которая сцепляет видимую таблицу(View) и адаптер
-            adapter.Fill(dt);
-
-            EmployeeDataGrid.DataContext = dt.DefaultView;
+            //Department
+            dtDep = new DataTable();
+            adapterDep.Fill(dtDep);
+            DepartamentDataGrid.DataContext = dtDep.DefaultView;
 
         }
 
+        #region Button Employee
+
+        
         private void BtnAddEmp_Click(object sender, RoutedEventArgs e)
         {
-            DataRow newRow = dt.NewRow();
+            DataRow newRow = dtEmp.NewRow();
             editWindow editWindow = new editWindow(newRow);
             editWindow.ShowDialog();
             if (editWindow.DialogResult.Value)
             {
-                dt.Rows.Add(editWindow.resultRow);
-                adapter.Update(dt);
+                dtEmp.Rows.Add(editWindow.resultRow);
+                adapterEmp.Update(dtEmp);
             }
         }
 
@@ -115,21 +156,65 @@ namespace WPF_Lesson7
             if (editWindow.DialogResult.HasValue && editWindow.DialogResult.Value)
             {
                 newRow.EndEdit();
-                adapter.Update(dt);
+                adapterEmp.Update(dtEmp);
             }
             else
             {
                 newRow.CancelEdit();
             }
         }
-
         private void BtnRevomeEmp_Click(object sender, RoutedEventArgs e)
         {
             DataRowView newRow = (DataRowView)EmployeeDataGrid.SelectedItem;
 
             newRow.Row.Delete();
-            adapter.Update(dt);
+            adapterEmp.Update(dtEmp);
         }
-    }
+        #endregion
+
+
+        #region Button Department
+
+        private void BtnAddDep_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow newRow = dtDep.NewRow();
+            EditDepWindow editDepWindow = new EditDepWindow(newRow);
+            editDepWindow.ShowDialog();
+            if (editDepWindow.DialogResult.Value)
+            {
+                dtDep.Rows.Add(editDepWindow.resultRow);
+                adapterDep.Update(dtDep);
+            }
+
     
+        }
+
+  
+
+        private void BtnChengeDep_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)DepartamentDataGrid.SelectedItem; //выбор строки
+            newRow.BeginEdit();
+            EditDepWindow editDepWindow = new EditDepWindow(newRow.Row);
+            editDepWindow.ShowDialog();
+            if (editDepWindow.DialogResult.HasValue && editDepWindow.DialogResult.Value)
+            {
+                newRow.EndEdit();
+                adapterDep.Update(dtDep);
+            }
+            else
+            {
+                newRow.CancelEdit();
+            }
+        }
+        private void BtnRevomeDep_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)DepartamentDataGrid.SelectedItem;
+            newRow.Row.Delete();
+            adapterDep.Update(dtDep);
+        }
+
+        #endregion
+    }
+
 }
